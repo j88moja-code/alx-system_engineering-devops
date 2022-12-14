b3229caf23c4f62093d209bc35b75ad7aebdd48b
j88moja-code <sitholent+github@gmail.com>
2022-12-14 13:46:10 -0800

    make corrections

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,6 @@
 # Changes SSH config file
 exec { 'echo':
   path    => 'usr/bin:/bin',
-<<<<<<< HEAD
   command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-=======
-  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
->>>>>>> fb720532fb1b307679ab1de085f9a9c99643deca
   returns => [0,1],
 }
